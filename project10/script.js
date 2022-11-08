ce33eaf2b21c95fa81293be26de3236a59344432
diveddy <116570828+diveddy@users.noreply.github.com>
2022-11-08 17:30:19 +0100

    Add files via upload

---
+++ project10/script.js
@@ -0,0 +1,95 @@
+// alert("Hello World") //
+// console.log("Hello Eddy") //
+
+//----------
+
+// let username = prompt("Введите своё имя")
+// alert(username)
+// console.log(username) 
+
+//------------------
+
+// let hello = "Hello "
+
+//console.log(hello + "World");
+
+//--------------------
+
+// let username = prompt("Введите своё имя")
+// let hello = "Hello, "
+// console.log(hello + username)
+
+//-------------
+
+// let username = prompt("Введите ваше имя")
+// console.log("Hello " + username)
+
+//-----------------
+
+// let isRaining = false
+// if(isRaining == true) {
+//   alert("Возьми зонт")
+//} else {
+//   alert("Погода сегодня солнечная, зонт не нужен")
+//}
+
+//-----------------
+
+/* let degree = 15
+if(degree > 15) {
+    alert("Выходи в футболке")
+} else if(degree < 3) {
+    alert("Надень куртку")
+} else {
+    alert("Надень кофту")
+}
+*/
+
+//----------------
+
+/* let price = 500
+    if(price > 700) {
+    alert("Ваша скидка - 20%")
+} else if(price > 500) {
+    alert("Ваша скидка - 15%")
+} else if(price < 500) {
+    alert("Скидки нет")
+}
+*/
+
+
+
+
+
+
+
+//ДЗ//
+
+/*
+let r = 20
+let g = 50
+let b = 200
+let rgb = "rgb("
+console.log(rgb + r + ", " + g + ", " + b + ")")
+*/
+
+
+/*
+let number = +prompt("Ввести число")
+    if(number > 100) {
+    alert(">")
+} else if(number < 100) {
+    alert("<")
+} else if(price = 100) {
+    alert("=")}
+*/
+
+/*
+let a = +prompt("Ввести число")
+let b = +prompt("Ввести второе число")
+let c = a + b
+let d = a - b
+let e = a * b
+let f = a / b
+console.log(c, + d, + e, + f);
+*/
